ca5a05806b4a6f0965e95b10718650aa26ff0bee
pyama <pyama@pepabo.com>
2023-08-26 03:00:45 +0900

    add permance test with nginx

---
+++ testdata/performance.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/url"
+
+	"github.com/k1LoW/rp"
+	"github.com/k1LoW/rp/testutil"
+)
+
+func main() {
+	r := testutil.NewRelayer(
+		map[string]*url.URL{
+			"127.0.0.1:18082": &url.URL{
+				Scheme: "http",
+				Host:   "127.0.0.1:18080",
+			},
+		},
+	)
+	s := rp.NewServer("127.0.0.1:18082", r)
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
+}
