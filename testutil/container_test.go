5ad3b169d84ee165a5f6ba49978971970876546a
k1low <k1low@pepabo.com>
2023-09-04 20:22:56 +0900

    Fix

---
+++ testutil/container_test.go
@@ -0,0 +1,31 @@
+package testutil
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestContainer(t *testing.T) {
+	_ = NewUpstreamEchoNGINXServer(t, "a.example.com")
+	upstreams := map[string]string{
+		"a.example.com": fmt.Sprintf("http://%s:80", "a.example.com"),
+	}
+	proxy := NewReverseProxyNGINXServer(t, "r.example.com", upstreams)
+	now := time.Now()
+	req, err := http.NewRequest("GET", proxy+"sleep", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Host = "a.example.com"
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	after := time.Now()
+	if after.Sub(now) < 1*time.Second {
+		t.Fatal("sleep.js is not working")
+	}
+}
