4cd6f5fd0683d9a96dd2450fe4bc9638a128cfd7
k1low <k1low@pepabo.com>
2023-09-04 20:49:24 +0900

    Fix cache setting of NGINX

--- testutil/container_test.go
+++ testutil/container_test.go
@@ -13,19 +13,37 @@ func TestContainer(t *testing.T) {
 		"a.example.com": fmt.Sprintf("http://%s:80", "a.example.com"),
 	}
 	proxy := NewReverseProxyNGINXServer(t, "r.example.com", upstreams)
-	now := time.Now()
-	req, err := http.NewRequest("GET", proxy+"sleep", nil)
-	if err != nil {
-		t.Fatal(err)
+	{
+		now := time.Now()
+		req, err := http.NewRequest("GET", proxy+"sleep", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Host = "a.example.com"
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+		after := time.Now()
+		if after.Sub(now) < 1*time.Second {
+			t.Fatal("sleep.js is not working")
+		}
 	}
-	req.Host = "a.example.com"
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer res.Body.Close()
-	after := time.Now()
-	if after.Sub(now) < 1*time.Second {
-		t.Fatal("sleep.js is not working")
+
+	{
+		req, err := http.NewRequest("GET", proxy+"sleep", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Host = "a.example.com"
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer res.Body.Close()
+		if res.Header.Get("X-Nginx-Cache") != "HIT" {
+			t.Fatal("NGINX cache is not working")
+		}
 	}
 }
