0986c26852a8380cc5eb3706449149543cc37b29
k1low <k1low@pepabo.com>
2023-09-29 15:42:46 +0900

    Add simple example

---
+++ example/go.mod
@@ -0,0 +1,7 @@
+module github.com/k1LoW/rp/example
+
+go 1.21.1
+
+require github.com/k1LoW/rp v0.4.3
+
+require github.com/ory/dockertest/v3 v3.10.0 // indirect
---
+++ example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/k1LoW/rp"
+)
+
+type myRelayer struct {
+}
+
+func (r *myRelayer) GetUpstream(req *http.Request) (*url.URL, error) {
+	u, _ := url.Parse("https://httpbin.org")
+	u.Path = "/anything" + req.URL.Path
+	return u, nil
+}
+func (r *myRelayer) Rewrite(*httputil.ProxyRequest) error {
+	return nil
+}
+
+func (r *myRelayer) GetCertificate(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	return nil, nil
+}
+
+func (r *myRelayer) RoundTrip(req *http.Request) (*http.Response, error) {
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+func main() {
+	var r rp.Relayer = &myRelayer{}
+	log.Fatal(rp.ListenAndServe(":8088", r))
+}
